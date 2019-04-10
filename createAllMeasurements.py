dc298f59b507cee6252dca892b55af845bd602e6
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2019-04-10 21:00:41 +0300

    add country 2l

--- createAllMeasurements.py
+++ createAllMeasurements.py
@@ -1,5 +1,6 @@
 import json
 import os
+import requests
 
 with open('speedtest_servers.json', 'r') as f:
     servers = json.load(f)
@@ -8,12 +9,12 @@ with open('countries.txt', 'r') as f:
     countries = f.readlines()
     countries = list(map(lambda s: s.strip(), countries))
 
-
 for source_country in countries:
     filename = "measurements/{}.txt".format(source_country)
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, 'w+') as f:
-        f.write("Measurements from: {}\n".format(source_country))
+        r = requests.get("https://restcountries.eu/rest/v2/name/{}".format(source_country))
+        f.write("Measurements from: {} ({})\n".format(source_country, r.json()[0]['alpha2Code']))
         f.write("+++++++++++++++++++++++++++++++++++++++++++\n")
         f.write("\n")
         for destination_country, destination_country_servers in servers.items():
