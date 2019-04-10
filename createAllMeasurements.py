0d12f6984a077be5b1a04aa58d971c6c3b107fba
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2019-04-10 20:47:37 +0300

    text file

---
+++ createAllMeasurements.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+with open('speedtest_servers.json', 'r') as f:
+    servers = json.load(f)
+
+with open('countries.txt', 'r') as f:
+    countries = f.readlines()
+    countries = list(map(lambda s: s.strip(), countries))
+
+
+for source_country in countries:
+    filename = "measurements/{}.txt".format(source_country)
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w+') as f:
+        f.write("Measurements from: {}\n".format(source_country))
+        f.write("+++++++++++++++++++++++++++++++++++++++++++\n")
+        f.write("\n")
+        for destination_country, destination_country_servers in servers.items():
+            f.write("Measurements to: {}\n".format(destination_country))
+            for speedtest_server in destination_country_servers:
+                f.writelines("Server: {}\n".format(speedtest_server['host']))
+            f.write("\n")
+            f.write("===============================================")
+            f.write("\n")
