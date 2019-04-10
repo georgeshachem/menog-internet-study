ce42544ebefa2b16125b85955e71f4a0c9435f72
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2019-04-10 23:34:56 +0300

    removed port

--- getSpeedTestServers.py
+++ getSpeedTestServers.py
@@ -5,15 +5,12 @@ import json
 with open('countries.txt', 'r') as f:
     countries = f.readlines()
 countries = list(map(lambda s: s.strip(), countries))
-print(countries)
-
 url = 'https://www.speedtestserver.com'
 data = requests.get(url)
 
 servers = dict()
 for country in countries:
     servers[country] = list()
-print(servers)
 html = BeautifulSoup(data.text, 'html.parser')
 
 all_servers = html.find('tbody').find_all('tr')
@@ -22,7 +19,7 @@ for server in all_servers:
     data = server.find_all('td')
     server_country = data[0].get_text()
     if server_country in countries:
-        servers[server_country].append({'host': data[3].get_text(), 'id': data[4].get_text()})
+        servers[server_country].append({'host': data[3].get_text().split(':')[0], 'id': data[4].get_text()})
 
 with open("speedtest_servers.json", "w") as f:
     json.dump(servers, f, indent=4, sort_keys=True)
