940fa3a7d0fec7e3c9140fbda32fa514043c1e7d
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2019-04-25 18:14:13 +0300

    Create stopAllMeasurement.py

---
+++ stopAllMeasurement.py
@@ -0,0 +1,27 @@
+import glob
+import json
+from ripe.atlas.cousteau import AtlasStopRequest
+
+ATLAS_STOP_API_KEY = ""
+
+failed_measurements = list()
+
+for filepath in glob.iglob('measurements/*.json'):
+    with open(filepath) as f:
+        data = json.load(f)
+    for k, v in data.items():
+        for elt in v:
+            try:
+                for measurement in elt['measurement_id']:
+                    atlas_request = AtlasStopRequest(msm_id=measurement, key=ATLAS_STOP_API_KEY)
+                    (is_success, response) = atlas_request.create()
+                    if not is_success:
+                        failed_measurements.append(measurement)
+            except KeyError:
+                pass
+        break
+    break
+
+with open('data/measurements-failed-to-stop.txt', 'w') as file:
+    for elt in failed_measurements:
+        file.write("%i\n" % elt)
