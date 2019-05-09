fa08e24f4e001ced947d70862f7540c3938d785c
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2019-05-09 14:39:19 +0300

    restruct

--- RiPE Atlas/checkSpeedTestServers.py
+++ RiPE Atlas/checkSpeedTestServers.py
@@ -1,5 +1,6 @@
 import json
 import socket
+import pyping
 
 with open("speedtest_servers.json", "r") as f:
     all_servers = json.load(f)
@@ -10,13 +11,11 @@ for country, country_servers in all_servers.items():
     working_servers[country] = list()
     for server in country_servers:
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            result = sock.connect_ex((server['host'], 80))
+            result = pyping.ping('82.102.232.203').ret_code
+            if result == 0:
+                working_servers[country].append(server)
         except Exception as e:
             print(e)
-            result = 1
-        if result == 0:
-            working_servers[country].append(server)
 
 with open("working_speedtest_servers.json", "w") as f:
     json.dump(working_servers, f, indent=4, sort_keys=True)
