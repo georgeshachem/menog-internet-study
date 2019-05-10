c4dc6adaa76c490a909411ec22cf6ef68a61d5d4
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2019-05-11 01:38:19 +0300

    updated speedtest servers countries name

--- menog-rtt-measure/getSpeedTestServers.py
+++ menog-rtt-measure/getSpeedTestServers.py
@@ -2,15 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-with open('countries.txt', 'r') as f:
-    countries = f.readlines()
-countries = list(map(lambda s: s.strip(), countries))
+with open('countries.json', 'r') as f:
+    countries = json.load(f)
+
 url = 'https://www.speedtestserver.com'
 data = requests.get(url)
 
 servers = dict()
-for country in countries:
-    servers[country] = list()
+for country, country_code in countries.items():
+    servers[country_code] = list()
 html = BeautifulSoup(data.text, 'html.parser')
 
 all_servers = html.find('tbody').find_all('tr')
@@ -18,8 +18,8 @@ all_servers = html.find('tbody').find_all('tr')
 for server in all_servers:
     data = server.find_all('td')
     server_country = data[0].get_text()
-    if server_country in countries:
-        servers[server_country].append({'host': data[3].get_text().split(':')[0], 'id': data[4].get_text()})
+    if server_country in list(countries.keys()):
+        servers[countries[server_country]].append({'host': data[3].get_text().split(':')[0], 'id': data[4].get_text()})
 
 with open("speedtest_servers.json", "w") as f:
     json.dump(servers, f, indent=4, sort_keys=True)
