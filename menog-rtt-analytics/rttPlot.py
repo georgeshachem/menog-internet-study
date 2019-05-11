a7f6edc818871a2e5b6e48c99e97ecfbb5e8b848
samerlahoud <samer@lahoud.fr>
2019-05-11 14:26:36 +0300

    integrate plot

--- menog-rtt-analytics/rttPlot.py
+++ menog-rtt-analytics/rttPlot.py
@@ -1,8 +1,36 @@
 import pandas as pd
 import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
+import networkx as nx
+import community
 
 rtt_min_df = pd.read_csv('graphs/rtt_min.csv', header = 0,index_col=0)
 ax = sns.heatmap(rtt_min_df, annot=True, fmt=".0f")
 plt.savefig('graphs/rtt_min_heatmap.eps')
+plt.savefig('graphs/rtt_min_heatmap.pdf')
+plt.show()
+
+rtt_min_dict = rtt_min_df.to_dict()
+G = nx.from_dict_of_lists(rtt_min_dict)
+for e1,e2 in list(G.edges):
+    G[e1][e2]['weight'] = rtt_min_df[e1][e2]
+
+#first compute the best partition
+partition = community.best_partition(G)
+print(partition)
+
+#drawing
+size = float(len(set(partition.values())))
+pos = nx.spring_layout(G)
+count = 0.
+#we use the list of partitions 
+for com in set(partition.values()) :
+    count = count + 1.
+    list_nodes = [nodes for nodes in partition.keys()
+                                if partition[nodes] == com]
+    nx.draw_networkx_nodes(G, pos, list_nodes, node_size = 20,
+                                node_color = str(count / size))
+
+
+nx.draw_networkx_edges(G, pos, alpha=0.5)
 plt.show()
